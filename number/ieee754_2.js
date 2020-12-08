f04b6ac15df98234d61c66cbab20ab1feb160407
Bruno Santos <brunosansp@outlook.com>
2020-12-07 21:47:38 -0300

    IEEE 754

---
+++ number/ieee754_2.js
@@ -0,0 +1,5 @@
+1/0;
+Math.pow(10, 1000); //  .pow é utilizado em operações de potenciação
+Number.MAX_VALUE * 2 // .MAX_VALUE é utilizado para imprimir o maior número possível na operação
+Math.log(0);
+-Number.MAX_VALUE * 2
---
+++ number/ieee754_3.js
@@ -0,0 +1,5 @@
+10 * "JavaScript";
+0/0;
+Math.sqrt(-9); // Raiz quadrada
+Math.log(-1); // Logarítmo
+parseFloat("JavaScript");
