ef63c5020b2e09630656066fbbf093fc8ad4ea67
Bruno Santos <brunosansp@outlook.com>
2020-12-07 22:38:36 -0300

    math api

---
+++ number/math_1.js
@@ -0,0 +1,8 @@
+Math.E; // número de Euler
+Math.LN10; // logarítmo natural de 10
+Math.LN2; // logarítmo natural de 2
+Math.LOG10E; // logarítmo de E na base 10
+Math.LOG2E; // logarítmo de E na base 2
+Math.PI;
+Math.SQRT1_2; // raiz quadrada de 1 sob 2
+Math.SQRT2; // raiz quadrada de 2 
---
+++ number/math_3.js
@@ -0,0 +1,9 @@
+Math.cbrt(8); // raiz cubica
+Math.cos(Math.PI/3); // cosseno
+Math.exp(1);
+Math.hypot(3,4);
+Math.log(1);
+Math.pow(2,10);
+Math.sin(Math.PI/2);
+Math.sqrt(4);
+Math.tan(Math.PI/4);
