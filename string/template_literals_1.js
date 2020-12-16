443043dc351782520c69b91a85c27d512e142e8a
Bruno Santos <brunosansp@outlook.com>
2020-12-16 00:12:52 -0300

    string template literals

---
+++ string/template_literals_1.js
@@ -0,0 +1,6 @@
+let host = "localhost";
+let port = "3000";
+let resource = "users";
+// let url = "https://" + host + ":" + port + "/" + resource;
+// template literal `${}`
+let url = `https://${host}:${port}/${resource}`;
---
+++ string/template_literals_6.js
@@ -0,0 +1,13 @@
+let monthsOfYear = `0 - Jann
+1 - Febn
+2 - Marn
+3 - Aprn
+4 - Mayn
+5 - Junn
+6 - Juln
+7 - Augn
+8 - Sepn
+9 - Octn
+10 - Novn
+11 - Dec`;
+console.log(monthsOfYear);
