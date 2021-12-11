d328838c7d851ef7f2d7689276eff6efcb64d3b7
brunosansp <brunosansp@outlook.com>
2021-12-11 14:29:34 -0300

    arrayIteration reduce, and exercise 5

--- collections/arrays/arrayIteration/example07_reduce.js
+++ collections/arrays/arrayIteration/example07_reduce.js
@@ -1,6 +1,6 @@
 /**
  * reduce: Retorna um valor com base no retorno da função passada por parâmetro
- * 
+ * o reduce recebe uma função com 2 parâmetros (total, e o elemento)
  */
 
 const frameworks = [
