18c1d797f03a95e1f89099b54903e09926bf6a22
brunosansp <brunosansp@outlook.com>
2021-12-26 18:44:06 -0300

    usando weakset

---
+++ collections/weakset/weakset_1.js
@@ -0,0 +1,6 @@
+/**
+ * Similar ao Set, permite apenas valores do tipo Object e
+ * mantém as referências de forma fraca, sendo volátil e não iterável.
+ */
+const ws1 = new WeakSet();
+console.log(ws1);
